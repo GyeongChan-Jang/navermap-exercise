6716ea6bf24e20e51c0bca8a8877c4aa436c0d00
chan <cham9994@gmail.com>
2023-01-14 21:23:59 +0900

    [기능] 커스텀 맵 컨트롤 클릭 이벤트 성공

--- components/MapChangeButton.ts
+++ components/MapChangeButton.ts
@@ -3,17 +3,16 @@ import control from '../public/styles/mapChangeButton.module.css'
 export const MapChangeButton = () => {
   return /* html */ ` 
   <div class=${control.changeButtons}>
-      <div class=${control.buttonContainer} id=mapChange>
-          <img src="/svg/map/flag.svg" width="20px" alt="랭킹" />
+      <div id=mapChangeButton class=${control.buttonContainer} id=mapChange>
+          <img src="/svg/flag.svg" width="20px" alt="랭킹" />
           <div class=${control.text}>깃발분석</div>
       </div>
-      
       <div class='${control.buttonContainer} mapChange'>
-          <img src="/svg/map/ranking.svg" width="20px" alt="랭킹" />
+          <img src="/svg/ranking.svg" width="20px" alt="랭킹" />
           <div class=${control.text}>맛집랭킹</div>
       </div>
       <div class='${control.buttonContainer} mapChange'>
-          <img src="/svg/map/polygon.svg" width="20px" alt="랭킹" />   
+          <img src="/svg/polygon.svg" width="20px" alt="랭킹" />   
           <span class=${control.text}>매출분포</span>
       </div>
   </div>`
