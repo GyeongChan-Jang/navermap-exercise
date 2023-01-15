f1669c644eb9a630b707e6fd207918c20a164c47
chan <cham9994@gmail.com>
2023-01-15 19:35:35 +0900

    [기능] 맵 전환 버튼 클릭 로직 및 스타일링 완료

--- components/MapChangeButton.ts
+++ components/MapChangeButton.ts
@@ -3,15 +3,21 @@ import control from '../public/styles/mapChangeButton.module.css'
 export const MapChangeButton = (pathname: string) => {
   return /* html */ ` 
   <div class=${control.changeButtons}>
-      <div id=flag-analysis class="${control.buttonContainer} ${pathname}">
+      <div id=flag-analysis class='${control.buttonContainer} ${
+    pathname === '/map/flag-analysis' ? control.selectedFlag : ''
+  }'>
           <img src="/svg/flag.svg" width="20px" alt="랭킹" />
           <div class=${control.text}>깃발분석</div>
       </div>
-      <div id=ranking-in-map class='${control.buttonContainer} ${pathname}'>
+      <div id=ranking-in-map class='${control.buttonContainer} ${
+    pathname === '/map/ranking-in-map' ? control.selectedRanking : ''
+  }'>
           <img src="/svg/ranking.svg" width="20px" alt="랭킹" />
           <div class=${control.text}>맛집랭킹</div>
       </div>
-      <div id=sales-distribution class='${control.buttonContainer} ${pathname}'>
+      <div id=sales-distribution class='${control.buttonContainer} ${
+    pathname === '/map/sales-distribution' ? control.selectedSales : ''
+  }'>
           <img src="/svg/polygon.svg" width="20px" alt="랭킹" />   
           <span class=${control.text}>매출분포</span>
       </div>
